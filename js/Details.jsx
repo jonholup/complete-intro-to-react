26603e262405ec7db2d38c66997104ea64d98535
Jonny Holupchinski <jonholup@gmail.com>
2017-05-31 14:46:28 -0500

    18

--- js/Details.jsx
+++ js/Details.jsx
@@ -1,16 +1,38 @@
 // @flow
 
-import React from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
 import Header from './Header';
+import Spinner from './Spinner';
 
-const Details = (props: { show: Show }) => {
+class Details extends Component {
+  state = {
+    apiData: { rating: ''}
+  };
+  componentDidMount () {
+    axios.get(`http://localhost:3000/${this.props.show.imdbID}`)
+    .then((response : { data: { rating: string}}) => {
+      this.setState({apiData: response.data})
+    })
+  }
+  props: {
+    show: Show
+  }
+  render() {
   const { title, description, year, poster, trailer } = props.show;
+  let ratingComponent;
+  if (this.state.apiData.rating) {
+    ratingComponent = <h3>{this.state.apiData.rating}</h3>
+  } else {
+    ratingComponent = <Spinner />
+  }
   return (
     <div className="details">
       <Header />
       <section>
         <h1>{title}</h1>
         <h2>({year})</h2>
+        {ratingComponent}
         <img
           src={`/public/img/posters/${poster}`}
           alt={`Poster for ${title}`}
