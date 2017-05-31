fe32e78538a0739309d0f45eb81f0cb215f8cda5
Jonny Holupchinski <jonholup@gmail.com>
2017-05-31 13:44:30 -0500

    16

--- js/Details.jsx
+++ js/Details.jsx
@@ -2,10 +2,26 @@
 
 import React from 'react';
 
-const Details = props => (
+const Details = props => {
+  const { title, description, year, poster, trailer } = props.show;
   <div className="details">
-    <pre><code>{JSON.stringify(props, null, 4)}</code></pre>
+    <header>
+      <h1>svideo</h1>
+    </header>
+    <section>
+      <h1>{title}</h1>
+      <h2>({year})</h2>
+      <img src={`/public/img/posters/${poster}`} alt={`Poster for ${title}`} />
+      <p>{description}</p>
+      </section>
+      <div>
+      <iframe
+      src={`https://youtube-nocookie.com/embed/${trailer}?rel=0&amp;controls=0&amp;showinfo=0`}
+      frameBorder="0"
+      allowFullScreen
+      title={`Trailer for ${title}`}
+      />
   </div>
-);
+};
 
 export default Details;
